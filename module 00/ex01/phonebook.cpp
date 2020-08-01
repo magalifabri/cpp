82f3020e43a49d5d263a1331f78961435528e6d8
Magali Fabri <magali_90@live.nl>
2020-08-01 10:37:40 +0200

    seems functional

--- module 00/ex01/phonebook.cpp
+++ module 00/ex01/phonebook.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include <unistd.h>
 #include <sstream>
-
+#include <unistd.h>
 
 #define RED "\033[0;31m"	
 #define B_RED "\033[1;31m"	
@@ -24,7 +23,7 @@ Bold: \u001b[1m
 Underline: \u001b[4m
 Reversed: \u001b[7m
 */
-
+// your excellence / holiness
 class contact_data
 {
 	public:
@@ -58,10 +57,69 @@ void loading()
 	std::cout << "\n";
 }
 
+int add_cmd(contact_data contacts[], int &number_of_contacts)
+{
+	static int i;
+
+	// FULL
+	if (number_of_contacts == 8)
+	{
+		std::cout << "Upgrade to EPIC FONEBUK PRO to store EVEN MORE contacts! Now for only €99.99/month!\n(Conditions apply)\n\n";
+		return (1);
+	}
+	// PROMPT user for contact info
+	std::cout << "Enter first name: ";
+	std::cin >> contacts[i].first_name;
+	contacts[i].first_name_short = contacts[i].first_name;
+	if (contacts[i].first_name.size() > 10)
+	{
+		contacts[i].first_name_short.resize(9);
+		contacts[i].first_name_short += '.';
+	}
+	std::cout << "Enter last name: ";
+	std::cin >> contacts[i].last_name;
+	contacts[i].last_name_short = contacts[i].last_name;
+	if (contacts[i].last_name.size() > 10)
+	{
+		contacts[i].last_name_short.resize(9);
+		contacts[i].last_name_short += '.';
+	}
+	std::cout << "Enter nickname: ";
+	std::cin >> contacts[i].nickname;
+	contacts[i].nickname_short = contacts[i].nickname;
+	if (contacts[i].nickname.size() > 10)
+	{
+		contacts[i].nickname_short.resize(9);
+		contacts[i].nickname_short += '.';
+	}
+	// std::cin >> contacts[i].nickname;
+	// std::cout << "Enter login: ";
+	// std::cin >> contacts[i].login;
+	// std::cout << "Enter postal address: ";
+	// std::cin >> contacts[i].postal_address;
+	// std::cout << "Enter email address: ";
+	// std::cin >> contacts[i].email_address;
+	// std::cout << "Enter phone number: ";
+	// std::cin >> contacts[i].phone_number;
+	// std::cout << "Enter birthday date: ";
+	// std::cin >> contacts[i].birthday_date;
+	// std::cout << "Enter favorite meal: ";
+	// std::cin >> contacts[i].favorite_meal;
+	// std::cout << "Enter underwear color: ";
+	// std::cin >> contacts[i].underwear_color;
+	// std::cout << "Enter darkest secret: ";
+	// std::cin >> contacts[i].darkest_secret;
+	std::cout << B_GREEN << "Contact saved, Highest Highness.\n\n" << RESET;
+	number_of_contacts++;
+	i++;
+	return (1);
+}
+
+
 int main()
 {
 	contact_data contacts[8];
-	int i;
+	// int i;
 	int j;
 	int number_of_contacts;
 	number_of_contacts = 0;
@@ -85,139 +143,141 @@ int main()
 	<< "To search for a contact, write 'SEARCH'\n"
 	<< "TO exit, write 'EXIT'\n"
 	<< "\n";
-	i = 0;
+	// i = 0;
+	std::string cmd;
 	while (1)
 	{
-		std::string cmd;
-		// prompt the user for input
+		// PROMPT the user for input
 		std::cout << "Command me, Divine Overlord: ";
-		// std::cin.sync();
-		// std::cin.clear();
-		// std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		// store the input
 		std::cin >> cmd;
-		// check what the input contains
+
+		// CHECK what the input contains
 		if (cmd.compare("EXIT") == 0 || cmd == "e") // TESTING: quicker cmd
 		{
 			std::cout << "I will be here upon your return, Magnificent Master.\n";
 			break ;
 		}
 		// Note: It makes more sense to me to truncate the input of the first name, last name and nickname here instead of when the contact list is returned for the SEARCH command. Because a contact is only added once, but it's likely to be displayed in the contact list many more times; so by doing a bit more work now, we save doing that bit of work over an over again in the future. Also, I see no point in having the full names stored in memory, when they will never be displayed anyway and also serve no other purpose.
+		
+		// ADD
 		else if (cmd.compare("ADD") == 0 || cmd == "a") // TESTING: quicker cmd
 		{
-			std::cout << "add command given\n"; // TESTING
-			std::cout << "Enter first name: ";
-			std::cin.ignore();
-			std::getline(std::cin, contacts[i].first_name);
-			contacts[i].first_name_short = contacts[i].first_name;
-			if (contacts[i].first_name.size() > 10)
-			{
-				contacts[i].first_name_short.resize(9);
-				contacts[i].first_name_short += '.';
-			}
-			std::cout << "Enter last name: ";
-			std::getline(std::cin, contacts[i].last_name);
-			contacts[i].last_name_short = contacts[i].last_name;
-			if (contacts[i].last_name.size() > 10)
-			{
-				contacts[i].last_name_short.resize(9);
-				contacts[i].last_name_short += '.';
-			}
-			std::cout << "Enter nickname: ";
-			std::getline(std::cin, contacts[i].nickname);
-			contacts[i].nickname_short = contacts[i].nickname;
-			if (contacts[i].nickname.size() > 10)
-			{
-				contacts[i].nickname_short.resize(9);
-				contacts[i].nickname_short += '.';
-			}
+			add_cmd(contacts, number_of_contacts);
+			// // std::cout << "add command given\n"; // TESTING
+			// // FULL
+			// if (number_of_contacts == 8)
+			// 	std::cout << "Upgrade to EPIC FONEBUK PRO to store EVEN MORE contacts! Now for only €99.99/month!\n (Conditions apply)\n\n";
+			// // PROMPT user for contact info
+			// std::cout << "Enter first name: ";
+			// std::cin >> contacts[i].first_name;
+			// contacts[i].first_name_short = contacts[i].first_name;
+			// if (contacts[i].first_name.size() > 10)
+			// {
+			// 	contacts[i].first_name_short.resize(9);
+			// 	contacts[i].first_name_short += '.';
+			// }
+			// std::cout << "Enter last name: ";
+			// std::cin >> contacts[i].last_name;
+			// contacts[i].last_name_short = contacts[i].last_name;
+			// if (contacts[i].last_name.size() > 10)
+			// {
+			// 	contacts[i].last_name_short.resize(9);
+			// 	contacts[i].last_name_short += '.';
+			// }
+			// std::cout << "Enter nickname: ";
 			// std::cin >> contacts[i].nickname;
-			// std::cout << "Enter login: ";
-			// std::cin >> contacts[i].login;
-			// std::cout << "Enter postal address: ";
-			// std::cin >> contacts[i].postal_address;
-			// std::cout << "Enter email address: ";
-			// std::cin >> contacts[i].email_address;
-			// std::cout << "Enter phone number: ";
-			// std::cin >> contacts[i].phone_number;
-			// std::cout << "Enter birthday date: ";
-			// std::cin >> contacts[i].birthday_date;
-			// std::cout << "Enter favorite meal: ";
-			// std::cin >> contacts[i].favorite_meal;
-			// std::cout << "Enter underwear color: ";
-			// std::cin >> contacts[i].underwear_color;
-			// std::cout << "Enter darkest secret: ";
-			// std::cin >> contacts[i].darkest_secret;
-			// std::cout << "The contact " << first_name << " " << last_name << " with the phone number " << phone_number << " has been added to the phone book\n";
-			std::cout << "Contact saved, Highest Highness.\n\n";
-			number_of_contacts++;
-			i++;
+			// contacts[i].nickname_short = contacts[i].nickname;
+			// if (contacts[i].nickname.size() > 10)
+			// {
+			// 	contacts[i].nickname_short.resize(9);
+			// 	contacts[i].nickname_short += '.';
+			// }
+			// // std::cin >> contacts[i].nickname;
+			// // std::cout << "Enter login: ";
+			// // std::cin >> contacts[i].login;
+			// // std::cout << "Enter postal address: ";
+			// // std::cin >> contacts[i].postal_address;
+			// // std::cout << "Enter email address: ";
+			// // std::cin >> contacts[i].email_address;
+			// // std::cout << "Enter phone number: ";
+			// // std::cin >> contacts[i].phone_number;
+			// // std::cout << "Enter birthday date: ";
+			// // std::cin >> contacts[i].birthday_date;
+			// // std::cout << "Enter favorite meal: ";
+			// // std::cin >> contacts[i].favorite_meal;
+			// // std::cout << "Enter underwear color: ";
+			// // std::cin >> contacts[i].underwear_color;
+			// // std::cout << "Enter darkest secret: ";
+			// // std::cin >> contacts[i].darkest_secret;
+			// std::cout << B_GREEN << "Contact saved, Highest Highness.\n\n" << RESET;
+			// number_of_contacts++;
+			// i++;
 		}
+		
+		// SEARCH
 		// else if (cmd.compare("SEARCH") == 0)
 		else if (cmd.compare("SEARCH") == 0 || cmd == "s") // TESTING: quicker cmd
 		{
-			// std::cout << "search command given\n";
 			if (number_of_contacts == 0)
-				std::cout << B_RED"I apologize, but no contacts were found..\n\n\033[0m";
+				std::cout << "I apologize, but no contacts were found..\n\n";
 			else
 			{
-				// print contact list
-				std::cout << "The following contacts were found:\n\n";
-				i = -1;
-				std::cout
-				<< std::setw(10) << "Index"
-				<< "|"
-				<< std::setw(10) << "First name"
-				<< "|"
-				<< std::setw(10) << "Last name"
-				<< "|" 
-				<< std::setw(10) << "Nickname"
-				<< "\n"
+				// PRINT contact list
+				std::cout << "You sought, and you found:\n\n"
+				<< std::setw(10) << "Index" << "|"
+				<< std::setw(10) << "First name" << "|"
+				<< std::setw(10) << "Last name" << "|" 
+				<< std::setw(10) << "Nickname" << "\n"
 				<< "–––––––––––––––––––––––––––––––––––––––––––\n";
 				j = -1;
 				while (++j < number_of_contacts)
 				{
 					std::cout
-					<< std::setw(10) << j
-					<< "|"
-					<< std::setw(10) << contacts[j].first_name_short
-					<< "|"
-					<< std::setw(10) << contacts[j].last_name_short
-					<< "|" 
+					<< std::setw(10) << j << "|"
+					<< std::setw(10) << contacts[j].first_name_short << "|"
+					<< std::setw(10) << contacts[j].last_name_short << "|" 
 					<< std::setw(10) << contacts[j].nickname_short << "\n";
 				}
-				// prompt user for index of contact
+				// PROMPT user for index of contact & process it
 				std::cout << "Enter the index number of the contact you wish to view: ";
-				std::string input;
 				int requested_index_number;
-				std::cin.clear();
-				std::getline(std::cin, input);
-				std::stringstream(input) >> requested_index_number;
-				// std::cin >> requested_index_number;
-				std::cout << requested_index_number << "\n";
-				if (requested_index_number < 0 && requested_index_number > 9)
-					std::cout << "I apologize, but that is not a number\n";
-				else if (requested_index_number > number_of_contacts - 1)
-					std::cout << "I'm sorry, there is no contact by that index number, Great Lord.\n";
+				if (std::cin >> requested_index_number)
+				{
+					// CHECK if requested index number is valid
+					// std::cout << "You requested: " << requested_index_number << "\n";
+					if (requested_index_number < 0 || requested_index_number > number_of_contacts - 1)
+						std::cout << "A number above -1 and below " << number_of_contacts << " would be most delightful.\n";
+					// DISPLAY contact info
+					else
+					{
+						std::cout << "Behold! The contact information for index number " << requested_index_number << ": \n";
+						std::cout << " - first name: " << contacts[requested_index_number].first_name << "\n";
+						std::cout << " - last name: " << contacts[requested_index_number].last_name << "\n";
+						std::cout << " - nickname: " << contacts[requested_index_number].nickname << "\n";
+						std::cout << " - login: " << contacts[requested_index_number].login << "\n";
+						std::cout << " - postal address: " << contacts[requested_index_number].postal_address << "\n";
+						std::cout << " - email address: " << contacts[requested_index_number].email_address << "\n";
+						std::cout << " - phone number: " << contacts[requested_index_number].phone_number << "\n";
+						std::cout << " - birthday date: " << contacts[requested_index_number].birthday_date << "\n";
+						std::cout << " - favorite meal: " << contacts[requested_index_number].favorite_meal << "\n";
+						std::cout << " - underwear color: " << contacts[requested_index_number].underwear_color << "\n";
+						std::cout << " - darkest secret: " << contacts[requested_index_number].darkest_secret << "\n\n";
+					}
+					//
+				}
+				else if (std::cin.bad())
+					std::cout << "IO error\n";
+				else if (std::cin.eof())
+					std::cout << "EOF reached (perhaps combined with a format problem)\n";
 				else
 				{
-					std::cout << "You requested: " << requested_index_number << "\n";
-					
+					std::cout << "format problem\n";
+					std::cin.clear(); // without it, rapid spam ensues 
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 				}
 			}
-			
 		}
 		else
 			std::cout << "I do not comprehend, Oh Wise One\n";
-		//		to add a contact: write "ADD" (without the quotations marks). Afterwards the user is prompted for a first name, a last name and a phone number
-		sleep(1);
-		//		to view a contact's number: write "SEARCH". Afterwards the user is prompted for a first name and then a last name. If a match if found, the phone number is returned, otherwise an error is returned
-		//		to exit: write "EXIT"
-		// compare input to ADD, SEARCH and EXIT
-		// if ADD: 
-		//		if 8 contacts are already present, deny request
-		//		if not, prompt user for more info
-		// if SEARCH: prompt user for more info, then look up a match
-		// if EXIT: exit
 	}
 }
