43a8ebc41a20fd8de21ed1bbf5983e868f257ff9
Magali Fabri <magali_90@live.nl>
2020-08-05 13:40:30 +0200

    seems functional

---
+++ module 01/ex04/ex04.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+
+int main()
+{
+	std::string str;
+	std::string *ptr;
+	std::string ref;
+
+	str = "HI THIS IS BRAIN";
+	std::cout << "str: " << str << std::endl;
+	std::cout << "&(address of)str: " << &str << std::endl;
+	ptr = &str;
+	std::cout << "ptr = &str\n";
+	std::cout << "ptr: " << ptr << std::endl;
+	std::cout << "*(value pointed to by)ptr: " << *ptr << std::endl;
+	ref = *ptr;
+	std::cout << "ref = *ptr\n";
+	std::cout << "ref: " << ref << std::endl;
+
+}
