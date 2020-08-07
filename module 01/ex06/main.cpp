0ecc88259a0c70078145c33f8fb3f182bac88dc6
Magali Fabri <magali_90@live.nl>
2020-08-07 07:29:24 +0200

    pre-structuring save

--- module 01/ex06/main.cpp
+++ module 01/ex06/main.cpp
@@ -1,44 +1,29 @@
 #include <iostream>
 #include <string>
 
-#define BLACK "\033[0;30m"
-#define RED "\033[0;31m"
-#define GREEN "\033[0;32m"
-#define YELLOW "\033[0;33m"
-#define BLUE "\033[0;34m"
-#define MAGENTA "\033[0;35m"
-#define CYAN "\033[0;36m"
-#define RESET "\033[0m"
-#define BOLD "\033[1m"
-#define UNDERLINE "\033[4m"
-#define REVERSED "\033[7m"
 
 // -------------------------- WEAPON CLASS ------------------------------------
 
-// Make a Weapon class, that has a type string, and a getType that returns a const reference to this string. Also has a setType
+
 class Weapon
 {
 public:
 	Weapon();
 	Weapon(std::string);
 	std::string type;
-	std::string *getType() const;
+	std::string getType() const;
 	void setType(std::string);
 };
-// default constructor
-Weapon::Weapon(void)
-{
-	std::cout << BLUE "Weapon default constructor() called\n" RESET;
-}
-// constructor overload with string parameter
+
 Weapon::Weapon(std::string type)
 {
-	std::cout << BLUE "Weapon constructor(std::string) called\n" RESET;
 	setType(type);
 }
 
-std::string *Weapon::getType(void) const
+std::string Weapon::getType(void) const
 {
+	const std::string &reference = type;
+	return (reference);
 }
 
 void Weapon::setType(std::string type)
@@ -50,40 +35,25 @@ void Weapon::setType(std::string type)
 // -------------------------- HUMANA CLASSES ------------------------------------
 
 
-// Now, create two classes, HumanA and HumanB, that both have a Weapon, a name, and an attack() function that displays something like: NAME attacks with his WEAPON_TYPE
 class HumanA
 {
 public:
-	HumanA();
-	HumanA(std::string, Weapon &);
-	Weapon *WeaponHumanA;
+	HumanA(std::string name, Weapon &wpn) : name(name), WeaponHumanA(wpn) {}
+	Weapon &WeaponHumanA;
 	std::string name;
 	void attack();
 };
-HumanA::HumanA(void)
-{
-	std::cout << BLUE "HumanA default constructor() called\n" RESET;
-}
-// "Weapon &weaponName" parameter so that the function isn't passed a copy of the weapon, but references the actual weapon itself.
-// "WeaponHumanA = &weaponName" variable assignment so that WeaponHumanA points to the value of weaponName and doesn't just copy it's value.
-// These two things are required for WeaponHumanA to also change when the instance of the Weapon itself is changed.
-HumanA::HumanA(std::string humanName, Weapon &weaponName)
-{
-	std::cout << BLUE "HumanA constructor(string, Weapon) called\n" RESET;
-	name = humanName;
-	WeaponHumanA = &weaponName;
-}
+
 
 void HumanA::attack(void)
 {
-	std::cout << name << " attacks with his " << WeaponHumanA->type << "\n";
+	std::cout << name << " attacks with his " << WeaponHumanA.type << "\n";
 }
 
 
 // -------------------------- HUMANB CLASSES ------------------------------------
 
 
-// Now, create two classes, HumanA and HumanB, that both have a Weapon, a name, and an attack() function that displays something like: NAME attacks with his WEAPON_TYPE
 class HumanB
 {
 public:
@@ -94,16 +64,9 @@ public:
 	void setWeapon(Weapon &);
 	void attack();
 };
-HumanB::HumanB(void)
-{
-	std::cout << BLUE "HumanB default constructor() called\n" RESET;
-}
-// "Weapon &weaponName" parameter so that the function isn't passed a copy of the weapon, but references the actual weapon itself.
-// "WeaponHumanB = &weaponName" variable assignment so that WeaponHumanB points to the value of weaponName and doesn't just copy it's value.
-// These two things are required for WeaponHumanB to also change when the instance of the Weapon itself is changed.
+
 HumanB::HumanB(std::string humanName)
 {
-	std::cout << BLUE "HumanB constructor(string) called\n" RESET;
 	name = humanName;
 }
 
@@ -114,21 +77,20 @@ void HumanB::setWeapon(Weapon &weaponName)
 
 void HumanB::attack(void)
 {
-	std::cout << name << " attacks with his " << WeaponHumanB->type << "\n";
+	std::cout << name << " attacks with his " << WeaponHumanB->getType() << "\n";
 }
 
 
 // -------------------------- MAIN ------------------------------------
 
 
-// Make it so the following code produces attacks with "crude spiked club" THEN "some other type of club", in both test cases:
 int main()
 {
 	{
 		Weapon club = Weapon("crude spiked club");
 		HumanA bob("Bob", club);
 		bob.attack();
-		club.setType("some other type of club");
+		club.setType("two-handed skull-crucher");
 		bob.attack();
 	}
 	{
@@ -136,7 +98,15 @@ int main()
 		HumanB jim("Jim");
 		jim.setWeapon(club);
 		jim.attack();
-		club.setType("some other type of club");
+		club.setType("two-handed skull-crucher");
 		jim.attack();
 	}
 }
+
+// In which case is it appropriate to store the Weapon as a pointer? As a reference? Why?
+
+// It seems to me like: References are just "simplified pointers" that are meant to take over certain jobs that pointers typically performed in C, but allow for simpler syntax. So use references when you can. The basic job that references are supposed to take over is passing a variable around that, when the value is modified anywhere, it changes everywhere. Just like you can do with a pointer, but now with cleaner looking code.
+
+// I would argue that the weapons (weaponHumanA, weaponHumanB) should be stored as pointers, and not as references, because they should be changeable: we need to be able to say "actually, let's create a different weapon, a sword, and have them butcher each other with that instead".
+// To my understanding, weaponHumanB isn't even able to be a reference. This is because a class reference member has to be initialized at class construction. But when HumanB is constructed, a Weapon isn't passed as argument to the constructor.
+// For practice purposes I did create the weaponHumanA variable as a reference, since weaponHumanA is initialized at class construction.
