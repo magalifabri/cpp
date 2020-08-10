ec5aa43516e87e2f80bc87cfb92ac461412262d0
Magali Fabri <magali_90@live.nl>
2020-08-10 12:38:32 +0200

    polish

--- module 02/ex01/Fixed.class.cpp
+++ module 02/ex01/Fixed.class.cpp
@@ -13,7 +13,8 @@ Fixed::Fixed(const Fixed &x)
 }
 
 /*
-Takes a constant integer as a parameter and converts it to the correspondant fixed(8) point value
+** Takes a constant integer as a parameter and converts it to the correspondant
+** fixed(8) point value
 */
 Fixed::Fixed(const int n)
 {
@@ -22,7 +23,8 @@ Fixed::Fixed(const int n)
 }
 
 /*
-Takes a constant floating point as a parameter and that converts it to the correspondant fixed(8) point value.
+** Takes a constant floating point as a parameter and that converts it to the
+** correspondant fixed(8) point value.
 */
 Fixed::Fixed(const float n)
 {
@@ -64,48 +66,25 @@ int Fixed::toInt(void) const
 }
 
 /*
-Output stream operator overload
-
-Inserts a floating point representation of the fixed point value into the parameter output stream.
-
-Function Summary:
-	- if the fractional bits (lowest 8) are all 0, we're (as good as) dealing with an integer (because the number doesn't have a fractional part) and we can just bitshift >> 8 (to undo the integer to fixed point value) and write
-	- if that is not the case
-		- write the integer part of the number (same process as above) followed by a '.'
-		- copy over only the fractional bits to long long fraction (we'll be working with an integer and not a floating point number, because floating point numbers cannot be bitshifted.)
-		- these fractional bits also needs to be >>8 (to undo the <<8 to convert it to fixedPointValue), but because we want 
-		- remove trailer 0's
-		- write fractional part
+** Output stream operator overload
+** 
+** Inserts a floating point representation of the fixed point value into the
+** parameter output stream.
+** 
+** Function Summary:
+** - if the fractional bits (lowest 8) are all 0, we're (as good as) dealing
+**   with an integer (because the number doesn't have a fractional part), so we
+**   can just bitshift >> 8 to undo the integer to fixed point value conversion
+**   and write.
+** - if that is not the case, we use "x.getRawBits() / pow(2, 8)" to undo the
+**   conversion to fixed point value, as this results in a floating point
+**   number, because pow returns a double.
 */
 std::ostream &operator<<(std::ostream &os, const Fixed &x)
 {
-	long long fraction;
-	int int_fraction;
-	float float_fraction;
-
 	if ((x.getRawBits() & 255) == 0)
 		os << (x.getRawBits() >> 8);
 	else
-	{
-		// os << (x.getRawBits() >> 8) << '.';
-		float_fraction = x.getRawBits();
-		// float_fraction = (float)int_fraction;
-		float_fraction /= pow(2, 8);
-		// fraction *= 100000000;
-		// fraction >>= 8;
-		// while (fraction % 10 == 0)
-    	// 	fraction /= 10;
-		os << float_fraction;
-	}
-	// else
-	// {
-	// 	os << (x.getRawBits() >> 8) << '.';
-	// 	fraction = x.getRawBits() & 255;
-	// 	fraction *= 100000000;
-	// 	fraction >>= 8;
-	// 	while (fraction % 10 == 0)
-    // 		fraction /= 10;
-	// 	os << fraction;
-	// }
+		os << x.getRawBits() / pow(2, 8);
 	return (os);
 }
